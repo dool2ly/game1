986415048ea02f95f5a2e4c65d65e24d77ec91cd
dool2ly <dool2ly@gmail.com>
2020-04-28 14:04:22 +0000

    Decorate register component

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-import './MainMenu.css'
+import './Menu.css'
 
 const MainMenuEl = ({text, link}) => (
   <Link to={link} style={{ textDecoration: 'none' }}>
-    <div className="menu-el">
+    <div className="menu-btn" style={{ height: '100px', width: '350px'}}>
       <p>{text}</p>
     </div>
   </Link>
@@ -14,7 +14,10 @@ const MainMenuEl = ({text, link}) => (
 class MainMenu extends Component {
   render() {
     return(
-      <div className="menu">
+      <div className="menu" style={{
+        width: '400px',
+        height: '270px'
+      }}>
         <MainMenuEl text={"New Game"} link="/register" />
         <MainMenuEl text={"Load Game"} link="/login" />
       </div>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
 
+import './Menu.css'
+
+const Form = ({value, button}) => (
+  <div className="form">
+    <input value={value} />
+    <div className="menu-btn">
+      {button}
+    </div>
+  </div>
+)
+
 class Register extends Component {
   render() {
     return(
-      <div>Register</div>
+      <div className="menu" style={{
+        width: '400px',
+        height: '400px'
+      }}>
+        <div className='title'>Register</div>
+        <div className='inputs'>
+          <Form value="TEST_1" button="Check"/>
+          <Form value="TEST_2" button="Submit"/>
+        </div>
+        <div className='go-home'>
+          <div className="menu-btn">Back</div>
+        </div>
+      </div>
     )
   }
 }
