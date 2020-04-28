67051938c8500eee9b118c76af3bedaa98911419
dool2ly <dool2ly@gmail.com>
2020-04-28 10:25:39 +0000

    Create link button of MainMenu component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import MainMenu from './components/MainMenu'
+import Register from './components/Register'
+import Login from './components/Login'
+
 import './App.css'
 
 class App extends Component {
@@ -10,6 +13,8 @@ class App extends Component {
       <BrowserRouter>
         <div className="main-box">
           <Route exact path="/" component={MainMenu} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
         </div>
       </BrowserRouter>
     )
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+const MainMenuEl = ({text, link}) => (
+  <li className="main-menu-el">
+    <Link to={link}>
+      <div className="main-menu-container">{text}</div>
+    </Link>
+  </li>
+)
 
 class MainMenu extends Component {
   render() {
     return(
-      <div>
-        MainMenu
-      </div>
+      <ul>
+        <MainMenuEl text={"New Game"} link="/register" />
+        <MainMenuEl text={"Load Game"} link="/login" />
+      </ul>
     )
   }
 }
